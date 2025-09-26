5ee18f64db6c91b934fea4907b956abb40baec3f
Quantum-Codes <87054411+Quantum-Codes@users.noreply.github.com>
2025-09-26 20:38:52 +0000

    api works

--- main.py
+++ main.py
@@ -4,14 +4,25 @@ import parser
 import enrich_papers
 import rag.main
 
-keyword_extractor.run_keyword_extraction("RAG")
-scraper.fetch_and_save_arxiv_papers()
-parser.parse_and_save_papers()
-enrich_papers.main()
-(vector_store, answer, documents) = rag.main.main()
+
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 
-print("=== RAG System Output ===\n"*10)
-print(f"Vector Store: {vector_store}")
-print(f"Answer: {answer}")
-print(f"Documents: {documents}")
+app = FastAPI()
+
+class PromptRequest(BaseModel):
+    prompt: str
+
+
+@app.post("/prompt")
+def query_rag_system(request: PromptRequest):
+    keyword_extractor.run_keyword_extraction(request.prompt)
+    scraper.fetch_and_save_arxiv_papers()
+    parser.parse_and_save_papers()
+    enrich_papers.main()
+    (vector_store, answer, documents) = rag.main.main()
+    return {
+        "message": answer,
+        "papers": documents
+    }
--- run.sh
+++
@@ -1,5 +0,0 @@
-poetry run python3 keyword_extractor.py
-poetry run python3 scraper.py
-poetry run python3 parser.py
-poetry run python3 enrich_papers.py
-poetry run python3 rag/main.py
